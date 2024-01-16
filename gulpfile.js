825607ceb22aa049feb86de531dcb8967b61a75b
narspi <ivanovmichurina@gmail.com>
2024-01-16 16:16:13 +0300

    add new scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,95 @@
-import gulp from 'gulp';
-import panini from 'panini';
-import browserSync  from 'browser-sync';
-import concat from 'gulp-concat';
-import sourcemaps from 'gulp-sourcemaps';
-import * as dartSass from 'sass'
-import gulpSass from 'gulp-sass';
+import gulp from "gulp";
+import panini from "panini";
+import browserSync from "browser-sync";
+import concat from "gulp-concat";
+import sourcemaps from "gulp-sourcemaps";
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
+import cleanCSS from "gulp-clean-css";
+import gulpif from "gulp-if";
 
-const tasks = gulp.registry().tasks();
-console.log(tasks);
+const isDevelopment = process.env.PRODUCTION === "development";
+const isTunnel = process.env.TUNNEL === "run";
 
 const scss = gulpSass(dartSass);
 
-const {src,dest,watch,series} = gulp;
+const { src, dest, watch, series } = gulp;
 const sync = browserSync.create();
 
 const htmlInclude = () => {
-    panini.refresh();
-    return src('src/html/pages/**/*.html')
-    .pipe(panini({
-        root: 'src/html/pages',
-        layouts: 'src/html/layouts',
-        partials: 'src/html/partials',
-        helpers: 'src/html/helpers/',
-        data: 'src/html/data/'
-    }))
-    .pipe(dest('dist/'))
-    .pipe(sync.stream())
-}
-
-const transportLibs = () => {
-    return src([
-        'node_modules/modern-normalize/modern-normalize.css',
-        'node_modules/swiper/swiper-bundle.css'
-    ])
-    .pipe(concat('_libs.scss'))
-    .pipe(dest('src/scss'))
-}
+  panini.refresh();
+  return src("src/html/pages/**/*.html")
+    .pipe(
+      panini({
+        root: "src/html/pages",
+        layouts: "src/html/layouts",
+        partials: "src/html/partials",
+        helpers: "src/html/helpers/",
+        data: "src/html/data/",
+      })
+    )
+    .pipe(dest("dist/"))
+    .pipe(sync.stream());
+};
 
 const createCss = () => {
-    return src('src/scss/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(scss({
-        outputStyle: 'expanded'
-    }))
-    .pipe(sourcemaps.write())   
-    .pipe(dest('dist/css'))
+  return src("src/scss/**/*.scss")
+    .pipe(gulpif(isDevelopment, sourcemaps.init()))
+    .pipe(
+      scss({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(
+      cleanCSS({
+        format: "beautify",
+      })
+    )
+    .pipe(gulpif(isDevelopment, sourcemaps.write()))
+    .pipe(dest("dist/css"))
     .pipe(sync.stream());
-}
+};
 
 const transportFonts = () => {
-    return src('./src/fonts/**/*.*')
-    .pipe(dest('dist/fonts'))
-}
+  return src("./src/fonts/**/*.*").pipe(dest("dist/fonts"));
+};
 
 const transportImg = () => {
-    return src('./src/img/**/*.*')
-    .pipe(dest('dist/img'))
-}
-
+  return src("./src/img/**/*.*").pipe(dest("dist/img"));
+};
 
 const server = () => {
-    sync.init({
-        server: './dist'
-    });
-    watch('./src/fonts/**/*.*', transportFonts);
-    watch('./src/img/**/*.*', transportImg);
-    watch('./src/html/**/*.html', htmlInclude);
-    watch('./src/scss/**/*.scss', createCss);
-}
+  sync.init({
+    server: "./dist",
+    tunnel: isTunnel ? true : null,
+  });
+  watch("./src/fonts/**/*.*", transportFonts);
+  watch("./src/img/**/*.*", transportImg);
+  watch("./src/html/**/*.html", htmlInclude);
+  watch("./src/scss/**/*.scss", createCss);
+};
 
-const serverTunnel = () => {
-    sync.init({
-        server: './dist',
-        tunnel: true
-    });
-    watch('./src/fonts/**/*.*', transportFonts);
-    watch('./src/img/**/*.*', transportImg);
-    watch('./src/html/**/*.html', htmlInclude);
-    watch('./src/scss/**/*.scss', createCss);
-}
+const buildServer = () => {
+  sync.init({
+    server: "./dist",
+    tunnel: true,
+  });
+};
 
-const defaultTask = series(htmlInclude,createCss,transportFonts,transportImg,server);
-const serverTunnelTask = series(htmlInclude,transportLibs,createCss,transportFonts,transportImg,serverTunnel);
-const buildTask = series(htmlInclude,transportLibs,createCss,transportFonts,transportImg);
+const defaultTask = series(
+  htmlInclude,
+  createCss,
+  transportFonts,
+  transportImg,
+  server
+);
 
-export { defaultTask as default, serverTunnelTask as tunnel, buildTask as build};
+
+const buildTask = series(htmlInclude, createCss, transportFonts, transportImg);
+
+const startServer = series(
+    buildTask,
+    buildServer
+);
+
+export { defaultTask as default, buildTask as build, startServer as start };
